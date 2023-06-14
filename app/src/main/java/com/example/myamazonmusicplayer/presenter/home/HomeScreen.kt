e21c33f7bb10a43c34d38bf53708dbaecc815944
tatsuya nakajima <fuzuki.1228@gmail.com>
2023-06-14 16:21:00 +0900

    いらないコメントアウト削除

--- app/src/main/java/com/example/myamazonmusicplayer/presenter/home/HomeScreen.kt
+++ app/src/main/java/com/example/myamazonmusicplayer/presenter/home/HomeScreen.kt
@@ -2,26 +2,20 @@ package com.example.myamazonmusicplayer.presenter.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myamazonmusicplayer.presenter.NavDestinationConst
-import com.example.myamazonmusicplayer.ui.theme.MyAmazonMusicPlayerTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -55,17 +49,6 @@ fun HomeScreen(
                 )
             }
         }
-
-//        Button(
-//            onClick = {
-//                viewModel.logout()
-//            },
-//            colors = ButtonDefaults.buttonColors(
-//                containerColor = Color.Transparent,
-//            )
-//        ) {
-//            Text("ログアウト")
-//        }
     }
 }
 
